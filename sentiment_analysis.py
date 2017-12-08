0d72196716fdc424f4c06861fd108c882e2a8b1e
Santhoshkrishnachaitanya Chelikavada <sc5612@nyu.edu>
2017-12-08 15:17:10 -0500

    API

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,20 +1,19 @@
-from pprint import pprint
 import json
+from pprint import pprint
+
+import pymysql
 import requests
 from bs4 import BeautifulSoup
-import pymysql
-
 
 BLOOMBERG_URL = "https://www.bloomberg.com/search?query="
 companies = ["Google", "Apple", "Snapchat", "Bloomberg"]
 
 
 class Spider:
-
     def __init__(self):
         self._key1 = "c31155fb4ef44e598697433926e764ae"
         self._key2 = "01572def178342179993aa4eef97d341"
-        self._sentiment_analysis_endpoint = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?Subscription-Key={key}"\
+        self._sentiment_analysis_endpoint = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?Subscription-Key={key}" \
             .format(key=self._key2)
 
     def _create_database(self):
@@ -48,16 +47,19 @@ class Spider:
 
         connection.close()
 
-
     def __get_sentiment(self, story):
-        payload = {'documents':[{'id':1, 'language':'en', 'text':story}]}
+        payload = {'documents': [{'id': 1, 'language': 'en', 'text': story}]}
         print(json.dumps(payload))
-        #headers = {'Ocp-Apim-Subscription-Key': self._key1}
+        # headers = {'Ocp-Apim-Subscription-Key': self._key1}
         url = self._sentiment_analysis_endpoint
-        request = requests.post(url, data=json.dumps(payload))
+        result = requests.post(url, data=json.dumps(payload))
+        json_result = json.loads(result.text)
 
-        json_result = json.loads(request.text)
-        return json_result.get("documents")[0].get("score")
+        try:
+            return json_result.get("documents")[0].get("score")
+        except:
+            return 0
+
 
     def get_links(self, company):
         return_array = []
@@ -74,19 +76,18 @@ class Spider:
 
         return return_array
 
-
     def get_content(self, company):
 
         links = self.get_links(company)
         return_content = dict()
         for link in links:
             content = requests.get(link).text
-            soup = BeautifulSoup(content,"html.parser")
+            soup = BeautifulSoup(content, "html.parser")
             story = ""
             paragraphs = soup.find_all("p")
             paragraphs = paragraphs[2:]
             for index, paragraph in enumerate(paragraphs):
-                if index>2:
+                if index > 2:
                     try:
                         story += paragraph.contents[0]
                         story += "\n"
@@ -95,9 +96,6 @@ class Spider:
 
             return_content[link] = story
         return return_content
-
-
-
 
     def get_sentiments(self):
 
@@ -121,21 +119,43 @@ class Spider:
                 if key is not None:
                     story = stories.get(key)
                     score = self.__get_sentiment(story)
-                    query = "INSERT IGNORE INTO ArticlesSentiment.Articles VALUES('{}','{}',{})".format(company,key, float(score))
+                    query = "INSERT IGNORE INTO ArticlesSentiment.Articles VALUES('{}','{}',{})".format(company, key,
+                                                                                                        float(score))
                     cursor.execute(query)
 
         connection.commit()
         cursor.close()
         connection.close()
 
+    def get_sentiments_for_company(self, company):
+
+        self._create_database()
+        self._create_table()
+
+        connection = pymysql.connect(host='34.235.205.203',
+                                     user='root',
+                                     password='dwdstudent2015',
+                                     db='ArticlesSentiment',
+                                     charset='utf8',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        cursor = connection.cursor()
+
+        stories = self.get_content(company=company)
+        stories = dict(stories)
+
+        for key in stories.keys():
+            if key is not None:
+                story = stories.get(key)
+                score = self.__get_sentiment(story)
+                query = "INSERT IGNORE INTO ArticlesSentiment.Articles VALUES('{}','{}',{})".format(company, key,
+                                                                                                    float(score))
+                cursor.execute(query)
+
+        connection.commit()
+        cursor.close()
+        connection.close()
 
 
-
-
-
-
-
-
-spider = Spider()
-pprint(spider.get_sentiments())
-
+if __name__ == "__main__":
+    spider = Spider()
+    pprint(spider.get_sentiments())
